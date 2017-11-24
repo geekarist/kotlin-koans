1f30e2a61570552f706bddd1d1a455752c82b14f
cpcp <geekarist@users.noreply.github.com>
2017-11-24 13:33:21 +0100

    Complete lesson3 until 10

--- lesson3/task10/src/Task.kt
+++ lesson3/task10/src/Task.kt
@@ -1,4 +1,12 @@
 // Return the set of products that were ordered by every customer
 fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
-    TODO()
+    val allProducts = customers
+            .flatMap { it.orders }
+            .flatMap { it.products }
+    val commonProducts = customers.fold(allProducts,
+            fun (productAccu: List<Product>, customer: Customer): List<Product> {
+                val customerProducts = customer.orders.flatMap { it.products }
+                return productAccu.intersect(customerProducts).toList()
+            })
+    return commonProducts.toSet()
 }
--- lesson3/task9/src/Task.kt
+++ lesson3/task9/src/Task.kt
@@ -1,2 +1,9 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
+    val (withMoreUnDelivered) = customers.partition {
+        val countDelivered = it.orders.count { it.isDelivered }
+        val countNotDelivered = it.orders.count { !it.isDelivered }
+        countNotDelivered > countDelivered
+    }
+    return withMoreUnDelivered.toSet()
+}

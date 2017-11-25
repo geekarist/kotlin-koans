5e3cf69698385191d5d06388f53c4dce898bf497
cpcp <geekarist@users.noreply.github.com>
2017-11-25 14:10:20 +0100

    Complete lesson3 until task11

--- lesson3/task11/src/Task.kt
+++ lesson3/task11/src/Task.kt
@@ -1,13 +1,21 @@
 // Return the most expensive product among all delivered products
 // (use the Order.isDelivered flag)
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
-    TODO()
+    return orders
+            .filter { it.isDelivered }
+            .flatMap { it.products }
+            .fold(Product("base", Double.MIN_VALUE), {
+                p1, p2 -> if (p1.price > p2.price) p1 else p2
+            })
 }
 
 // Return how many times the given product was ordered.
 // Note: a customer may order the same product for several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+    return customers
+            .flatMap { it.orders }
+            .flatMap { it.products }
+            .count { it === product }
 }
 
 /* TODO */

17cb44064b3dec720f0f9853f472c01bc513b3cd
cpcp <geekarist@users.noreply.github.com>
2017-11-27 13:17:21 +0100

    Complete until lesson6/task1

--- lesson6/task1/src/Task.kt
+++ lesson6/task1/src/Task.kt
@@ -1,6 +1,17 @@
 import java.util.*
 
-fun partitionTo() = TODO()
+fun <T> Collection<T>.partitionTo(left: MutableCollection<T>,
+                                  right: MutableCollection<T>,
+                                  predicate: (T) -> Boolean):
+        Pair<Collection<T>, Collection<T>> {
+
+    for (item in this) {
+        if (predicate(item)) left.add(item)
+        else right.add(item)
+    }
+
+    return Pair(left, right)
+}
 
 fun partitionWordsAndLines() {
     val (words, lines) = listOf("a", "a b", "c", "d e").
@@ -11,7 +22,7 @@ fun partitionWordsAndLines() {
 
 fun partitionLettersAndOtherSymbols() {
     val (letters, other) = setOf('a', '%', 'r', '}').
-            partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+            partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
     letters == setOf('a', 'r')
     other == setOf('%', '}')
 }
